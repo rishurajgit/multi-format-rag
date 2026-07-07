f7e2d2bc0402d7c37eb23b90204884aa5932b7d3
rishurajgit <rajrishu.cs@gmail.com>
2026-07-07 12:57:45 +0530

    Implementation of Task 2

---
+++ app/services/retriver_service.py
@@ -0,0 +1,17 @@
+from app.db.chroma_db import ChromaDB
+
+class RetrieverService:
+    def __init__(self):  #automatically connect to database
+        self.vector_store = ChromaDB().get_vector_store()
+        
+    def retrieve_documents(self, query: str, k: int = 4):
+        """
+        Retrieve the most relevant document chunks.
+        """
+        
+        documents = self.vector_store.similarity_search(
+            query = query,
+            k = k
+        )
+        
+        return documents 
